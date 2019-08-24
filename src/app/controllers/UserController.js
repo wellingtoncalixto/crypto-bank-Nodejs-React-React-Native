54914abd4c09b6346918c2b34b382b3ef5e4a882
Wellington Calixto <wac.monti@outlook.com>
2019-08-24 15:21:06 -0300

    criação do modo de update de usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,22 @@
+import * as yup from 'yup';
+
 import User from '../model/User';
 
 class UserController {
   async store(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string().requered(),
+      email: yup.string().requered(),
+      password: yup
+        .string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'error na validação' });
+    }
+
     const UserExist = await User.findOne({ where: { email: req.body.email } });
 
     if (UserExist) {
@@ -12,6 +27,52 @@ class UserController {
 
     return res.json({ id, name, email, saldo });
   }
-}
 
+  async update(req, res) {
+    const schema = yup.object().shape({
+      name: yup.string(),
+      email: yup.string(),
+      oldpassword: yup
+        .string()
+        .required()
+        .min(6),
+      password: yup
+        .string()
+        .required()
+        .min(6)
+        .when('oldpassword', (oldPassword, field) =>
+          oldPassword ? field.requered() : field
+        ),
+      confirmPassword: yup
+        .string()
+        .when('password', (password, field) =>
+          password ? field.required.oneOf([yup.ref('password')]) : field
+        ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.json({ error: 'error na validação' });
+    }
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const exist = await User.findOne({ where: { email } });
+
+      if (exist) {
+        return res
+          .status(400)
+          .json({ error: 'Outro usuario já esta usando esse email' });
+      }
+    }
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: 'Password não combinam' });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({ id, name, email });
+  }
+}
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,15 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import Middleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/login', SessionController.store);
 routes.post('/create', UserController.store);
+
+routes.use(Middleware);
+
+routes.put('/user/update', UserController.update);
 
 export default routes;
